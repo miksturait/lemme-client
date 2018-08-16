304f72caec5d15511d50e4eec10199a75590702f
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2018-08-16 17:05:05 +0200

    Simple display seats

--- app/components/tables-preview/component.js
+++ app/components/tables-preview/component.js
@@ -1,4 +1,16 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  init() {
+    this._super(...arguments);
+
+    this.set('tables', []);
+  },
+
+  actions: {
+    addTable() {
+      const newTable = { seatsCount: 2 };
+      this.tables.pushObject(newTable);
+    }
+  }
 });

f367d97cc7ceee6a14ef1d56b2f8f7c71eac5695
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2018-08-23 17:23:52 +0200

    Save tables in database

--- app/components/tables-preview/component.js
+++ app/components/tables-preview/component.js
@@ -1,17 +1,21 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  store: service(),
+
   init() {
     this._super(...arguments);
 
-    this.set('tables', []);
     this.set('seatsCount', 2);
   },
 
   actions: {
     addTable() {
       const newTable = { seatsCount: this.seatsCount };
-      this.tables.pushObject(newTable);
+      this.get('store')
+        .createRecord('table', newTable)
+        .save();
 
       this.set('seatsCount', 2);
       this.set('showAddTableForm', false);

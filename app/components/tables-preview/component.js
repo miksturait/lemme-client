ac0f4e399c7629588b6a545a989c67cc056bcbf9
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2018-08-23 19:00:12 +0200

    Display seats through model

--- app/components/tables-preview/component.js
+++ app/components/tables-preview/component.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { range } from 'lodash';
+import { filterBy } from '@ember/object/computed';
 
 export default Component.extend({
   store: service(),
@@ -11,47 +12,36 @@ export default Component.extend({
     this.set('seatsCount', 2);
   },
 
+  filteredTables: filterBy('tables', 'isDeleted', false),
+
   actions: {
     addTable() {
-      this.get('store')
+     const table = this.get('store')
         .createRecord('table', {name: '1'})
-        .save()
-        .then((table) => {
-           range(this.seatsCount).map(() => {
-            return this.get('store').createRecord('seat', {
-              active: false,
-              ready: false,
-              table
-            })
-            .save()
-             .then((seat) => {
-                table.get('seats').pushObject(seat);
-                table.save();
-             });
-          });
-        })
 
-      // const seatsPromises = range(this.seatsCount).map(() => {
-      //   return this.get('store').createRecord('seat', {
-      //     active: false,
-      //     ready: false,
-      //     table
-      //   }).save();
-      // });
+      const seatsPromises = range(this.seatsCount).map(() => {
+        return this.get('store').createRecord('seat', {
+          active: false,
+          ready: false,
+          table
+        }).save();
+      });
 
-      // Promise.all(seatsPromises)
-      //   .then((seats) => {
-      //     table.set('seats', seats);
-      //     table.save();
+      Promise.all(seatsPromises)
+        .then((seats) => {
+          table.set('seats', seats);
+          table.save();
 
-      //     this.set('seatsCount', 2);
-      //     this.set('showAddTableForm', false);
-      //   });
+          this.set('seatsCount', 2);
+          this.set('showAddTableForm', false);
+        });
     },
 
     removeTable(table) {
-      table.get('seats').forEach(seat => seat.destroyRecord());
-      table.destroyRecord();
+      Promise.all(table.get('seats').map((seat) => seat.destroyRecord()))
+        .then(() => {
+          table.destroyRecord();
+        })
     },
 
     showForm() {

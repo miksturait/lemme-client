a32e2c1df4c6769e0f1d138d8985af35e3eea0fb
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2018-08-23 18:34:59 +0200

    Add creating table with seats as relationship

--- app/components/tables-preview/component.js
+++ app/components/tables-preview/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { range } from 'lodash';
 
 export default Component.extend({
   store: service(),
@@ -12,16 +13,29 @@ export default Component.extend({
 
   actions: {
     addTable() {
-      const newTable = { seatsCount: this.seatsCount };
-      this.get('store')
-        .createRecord('table', newTable)
-        .save();
+      const table = this.get('store')
+        .createRecord('table')
 
-      this.set('seatsCount', 2);
-      this.set('showAddTableForm', false);
+      const seatsPromises = range(this.seatsCount).map(() => {
+        return this.get('store').createRecord('seat', {
+          active: false,
+          ready: false,
+          table
+        }).save();
+      });
+
+      Promise.all(seatsPromises)
+        .then((seats) => {
+          table.set('seats', seats);
+          table.save();
+
+          this.set('seatsCount', 2);
+          this.set('showAddTableForm', false);
+        });
     },
 
     removeTable(table) {
+      table.get('seats').forEach(seat => seat.destroyRecord());
       table.destroyRecord();
     },
 
--- app/models/table.js
+++ app/models/table.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  seatsCount: DS.attr('number')
+
+  seats: DS.hasMany('seat')
 });
